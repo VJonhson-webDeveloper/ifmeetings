4a6ea6d767b800ce5babf1593ea6cef7922bc384
Victor Jonhson Aciole do Nascimento <101227209+VJonhson-webDeveloper@users.noreply.github.com>
2022-10-31 13:57:20 -0300

    Adição do método responsável por salvar os cursos cadastrados no formulário de cadastro, bem como garantindo o envio da mensagem de sucesso para a página HTML.

--- src/main/java/br/edu/ifrn/teste/ifmeetings/controller/curso/CadastroCursoController.java
+++ src/main/java/br/edu/ifrn/teste/ifmeetings/controller/curso/CadastroCursoController.java
@@ -1,9 +1,16 @@
 package br.edu.ifrn.teste.ifmeetings.controller.curso;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.edu.ifrn.teste.ifmeetings.Dominio.Curso;
 
@@ -17,5 +24,41 @@ public class CadastroCursoController {
         model.addAttribute("curso", new Curso());
 
         return "curso/cadastro";
+    }
+
+    @SuppressWarnings("unchecked")
+    @PostMapping("/cadastrar")
+    public String cadastrarCurso(Curso curso, RedirectAttributes attr, HttpSession memoria) {
+
+        //acessando dados da memória 
+
+        //pegando o último id de Curso cadastrado
+        Integer idCurso = (Integer) memoria.getAttribute("idCurso");
+
+        //Criando lista de cursos já cadastrados na memória
+        List<Curso> cursoscadastrados = (List<Curso>) memoria.getAttribute("cursoscadastrados");
+
+        //se o id do Curso for igual a null (o), então iremos cadastrar o primeiro curso
+        //por isso ele receberá o id = 1
+        if (idCurso == null) {
+            idCurso = 1;
+        }
+
+        //se não houver nenhum curso cadastrado teremos que criar uma nova lista de cursos cadastrados para armazena-los
+        if (cursoscadastrados == null) {
+            cursoscadastrados = new ArrayList<>();
+        }
+
+        //após as verificações
+
+        //salvando o id do curso cadastrado e adiciona-o na lista de cursos cadastrados
+        curso.setIdCurso(idCurso);
+        cursoscadastrados.add(curso);
+        idCurso++;
+
+        //enviandp mensagem de sucesso
+        attr.addFlashAttribute("msgSucesso", "Curso cadastrado com sucesso!");
+
+        return "redirect:/cursos/cadastro";
     }
 }
